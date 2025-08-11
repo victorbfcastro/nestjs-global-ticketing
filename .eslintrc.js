7f063a3f6f42846b29416c4812b4ef0edf435642
victorbfcastro <victorbfc@hotmail.com>
2025-08-11 15:59:12 -0300

    feat: first commit & setup

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  plugins: ["@typescript-eslint", "import"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "prettier"
+  ],
+  rules: {
+    "import/order": ["warn", { "newlines-between": "always" }]
+  },
+  ignorePatterns: ["dist", "node_modules"]
+};
